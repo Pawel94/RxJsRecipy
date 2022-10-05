468b8fdc514a979eae01545124e2b24e52515cdf
DESKTOP-C8761K8\Pablo <iwap18@gmail.com>
2022-10-05 16:00:12 +0200

    Added animations to notification system

--- Front-end/RecipesRxJs/src/app/notification/notification.component.ts
+++ Front-end/RecipesRxJs/src/app/notification/notification.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { NotificationService } from '../services/notification-service/notification.service';
-
+import { animate, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0.5, transform: 'translateX(500px)' }),
+        animate('800ms', style({ opacity: 1, transform: 'translateX(110)' })),
+      ]),
+      transition(':leave', [
+        animate('800ms', style({ opacity: 0, transform: 'translateX(100px)' })),
+      ]),
+    ]),
+  ],
 })
 export class NotificationComponent implements OnInit {
   successMessage$ = this.notificationService.successMessageSubject$.pipe(
